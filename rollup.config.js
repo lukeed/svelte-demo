4273ce22e610b9bb8e4afd14e87fb8b5374bbce2
Luke Edwards <luke.edwards05@gmail.com>
2021-10-07 13:25:04 -0700

    chore: bump dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import replace from '@rollup/plugin-replace';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -17,14 +18,21 @@ export default {
 	preserveEntrySignatures: false,
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('bundle.css');
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production,
 			}
 		}),
+
+		css({
+			output: 'bundle.css',
+		}),
+		// 	// we'll extract any component CSS out into
+		// 	// a separate file — better for performance
+		// 	css: css => {
+		// 		css.write('bundle.css');
+		// 	}
+		// }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
@@ -35,6 +43,7 @@ export default {
 		commonjs(),
 
 		replace({
+			preventAssignment: true,
 			'process.env.NODE_ENV': JSON.stringify(production ? 'production' : 'development')
 		}),
 
